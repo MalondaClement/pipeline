23e6856607eb78f4c2831867eb9b1b972f77963d
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2021-07-19 21:26:39 +0200

    fix num_classes for models

--- trainDenseASPP.py
+++ trainDenseASPP.py
@@ -41,7 +41,7 @@ def main():
         os.makedirs(args.save_path + '/results_color_test')
 
     # Set model
-    model = DenseASPP(Model_CFG, n_class=20)
+    model = DenseASPP(Model_CFG, n_class=30)
 
     # Check if cuda is available to push model on GPU
     if torch.cuda.is_available():
