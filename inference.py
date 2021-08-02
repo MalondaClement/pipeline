b9387d14ddc259e15585e5e27a126b9adbe2528f
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2021-08-02 10:53:00 +0200

    add inference.py file

---
+++ inference.py
@@ -0,0 +1,83 @@
+#
+#  train.py
+#
+#  Created by Clément Malonda on 02/08/2021.
+
+import os
+import time
+import torch
+import numpy as np
+from torchvision.transforms import ToTensor, ToPILImage
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from helpers.ARGS import ARGS
+from learning.tunnel import Tunnel
+from models.utils import get_model
+from helpers.helpers import vislbl
+
+def main():
+    # Get tunnel dataset
+    Dataset = Tunnel
+
+    # Set up execution arguments
+    args = ARGS("DenseASPP121", "tunnel", len(Dataset.validClasses), batch_size=2, epochs=10)
+
+    model, args = get_model(args)
+
+    checkpoint = torch.load("save/DenseASPP121/2021-08-02-1627891784/best_weights.pth.tar", map_location=torch.device('cpu'))
+
+    args.save_path = os.path.join("inference", "DenseASPP121/2021-08-02-1627891784")
+
+    if not os.path.isdir(args.save_path):
+        os.makedirs(args.save_path)
+
+    state = checkpoint["model_state_dict"]
+    new_state = {}
+    for key in state:
+        new_state[key[7:]] = state[key]
+
+    model.load_state_dict(new_state)
+    model.eval()
+
+    times = list()
+
+    start = time.time()
+    for i, file in enumerate(os.listdir("tunnel/images")):
+        start = time.time()
+
+        img = Image.open(os.path.join("tunnel/images",file))
+        img = np.array(img)
+        img = img[:,:,:3]
+
+        input = ToTensor()(img)
+        input = input.unsqueeze(0)
+        with torch.no_grad():
+            output = model(input)
+        if args.is_pytorch_model:
+            output = output["out"]
+        preds = torch.argmax(output, 1)
+        pred = preds[0,:,:].cpu()
+        pred_color = vislbl(pred, Dataset.mask_colors)
+        pred_color = Image.fromarray(pred_color.astype("uint8"))
+        end = time.time()
+        times.append(int(end - start))
+        print("Temps d'inférence sur l'image {}: {} min {} s".format(file, int((end - start)/60), int((end - start)%60)))
+
+        fig, (ax0, ax1, ax2) = plt.subplots(1, 3)
+        fig.set_size_inches(18.5, 10.5)
+        ax0.imshow(img)
+        ax0.set_title("Image d'origine")
+        ax1.imshow(pred_color)
+        ax1.set_title("Prédiction")
+        ax2.imshow(img)
+        ax2.imshow(pred_color, alpha=0.6)
+        ax2.set_title("Superposition de l'image avec la prédiction")
+        fig.savefig(os.path.join("inference", "DenseASPP121/2021-08-02-1627891784", file))
+
+    mean = int(np.mean(times))
+    print("Temps d'inférence moyen sur les images : {} min  {} s".format(int(mean/60), mean%60))
+    plt.show()
+
+if __name__ == "__main__":
+    main()
