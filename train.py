7d1df52f04bddd9b9f2a89a7a34bdcbe03ff5ecf
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2021-08-10 14:09:31 +0200

    test training on microcity update batch_size

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ def main():
     Dataset = MiniCity
 
     # Set up execution arguments
-    args = ARGS("DeepLabV3_Resnet50", "microcity", len(Dataset.validClasses), labels_type="csv", batch_size=4, epochs=40)
+    args = ARGS("DeepLabV3_Resnet50", "microcity", len(Dataset.validClasses), labels_type="csv", batch_size=2, epochs=40)
 
     # Get model
     model, args = get_model(args)
